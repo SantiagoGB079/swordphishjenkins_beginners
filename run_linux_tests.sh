b3bab27d1eaa751ea22d097243e9b2c6150c01f2
Santiago Gomez <santiago.gomez@appgate.com>
2022-03-17 15:33:23 -0500

    Se corrige comando para ejecutar el dockerr-compose en el pipeline

--- run_linux_tests.sh
+++ run_linux_tests.sh
@@ -2,5 +2,6 @@ echo 'CORRIENDO PRUEBAS'
 echo 'CREANDO IMAGEN DOCKER'
 docker build -t swtestsjar:1.0.0 ./swordphish_test/
 echo 'IMAGE CREATED SUCCESSFULLY'
-docker-compose up -d -f ./swordphish_test/
+docker-compose -f ./swordphish_test/docker-compose.yml up -d
 #mvn clean -f ./swordphish_test/pom.xml verify
+
