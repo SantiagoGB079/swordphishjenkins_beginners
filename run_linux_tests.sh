13b1c088a288291b44f3123ec130aa98d902e3c6
Santiago Gomez <santiago.gomez@appgate.com>
2022-03-17 11:44:45 -0500

    edit line that generate image docker and run docker-compose exec

--- run_linux_tests.sh
+++ run_linux_tests.sh
@@ -1,6 +1,6 @@
 echo 'CORRIENDO PRUEBAS'
 echo 'CREANDO IMAGEN DOCKER'
-docker build -t swtestsjar:1.0.0 ./swordphish_test/
+docker exec build -t swtestsjar:1.0.0 ./swordphish_test/
 echo 'IMAGE CREATED SUCCESSFULLY'
-docker-compose up -d -f ./swordphish_test/
+docker-compose exec up -d -f ./swordphish_test/
 #mvn clean -f ./swordphish_test/pom.xml verify
