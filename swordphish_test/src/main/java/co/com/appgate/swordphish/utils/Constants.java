39be774ce21a6bfd3a19d8349c6ce7e69fe53ffe
Santiagogb0795 <73368711+SantiagoGB079@users.noreply.github.com>
2022-03-23 17:06:36 -0500

    Se cambian topics de Beta

--- swordphish_test/src/main/java/co/com/appgate/swordphish/utils/Constants.java
+++ swordphish_test/src/main/java/co/com/appgate/swordphish/utils/Constants.java
@@ -3,8 +3,8 @@ package co.com.appgate.swordphish.utils;
 public class Constants {
 
     public static final String BOOTSTRAP_SERVERS = "dtp-integration-kafka.dtp.appgate.com:9092";
-    public static final String TOPIC_SALIDA = "dms-swordphish3-url-score";
-    public static final String TOPIC_ENTRADA = "dms-swordphish3-url";
+    public static final String TOPIC_SALIDA = "dms-swordphish-url-score";
+    public static final String TOPIC_ENTRADA = "dms-swordphish-url";
     public static final String GROUP_ID = "my-fourth-application";
 
 }
